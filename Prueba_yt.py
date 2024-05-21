8f423ef030d62db200b31a89849c688f8f67afb6
Jackson <jacksontorres88@gmail.com>
2024-05-21 19:31:25 -0300

    Se agrega el codigo de la prueba de busqueda de video con google y youtube

---
+++ Prueba_yt.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+#instaciamos el driver de chrome
+driver = webdriver.Chrome()
+
+#navega por la url
+driver.get("https://www.google.com")
+
+#maximizar ventana
+driver.maximize_window()
+
+#buscar elemento  por su nombre
+search_box = driver.find_element(By.NAME,"q")
+search_box.click()
+search_box.send_keys("Slipknot"+Keys.ENTER)
+#hace clik 
+search_video = driver.find_element(By.LINK_TEXT, "Videos").click()
+play_video = driver.find_element(By.PARTIAL_LINK_TEXT, "Slipknot - Psychosocial")
+play_video.click()
+
+#se agrega mas tiempo para poder interactuar con la pantalla del video
+time.sleep(120)
+
+driver.quit()
